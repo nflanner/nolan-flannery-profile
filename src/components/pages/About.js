977a845c31731117197dcd9caad89633fb58180f
Nolan Flannery <nolanflannery@gmail.com>
2022-08-30 13:19:54 -0700

    about me done

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,17 +1,42 @@
 import React from 'react';
+import nolanPortrait from './../../Assets/Images/Nolan.jpg';
 
 export default function About() {
   return (
-    <div>
-      <h1>About Page</h1>
-      <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
-      </p>
+    <div className='row'>
+      <div className='col-12'>
+        <h1 className='title'>About Me</h1>
+      </div>
+      <div className='col-3'>
+        <img src={nolanPortrait} alt={'Nolan portrait'} className="img-thumbnail"/>
+      </div>
+      <div className='col-8'>
+        <p>
+          I have always had an interest in mathematics which led me to engineering in college. 
+          I held two engineering internship positions at Boeing during my undergraduate but found 
+          that physics and mathematics was my true passion in my studies. After graduating with a BA 
+          in mathematics and BS in Physics from University of Washington, I then decided to take a 
+          year off to work at the applied physics lab at UW. This further excelled my engineering 
+          experience via circuit design, sensor calibration, lab testing, and analysis. As a graduate 
+          student of physics at Northeastern, I eventually discovered a new interest in machine 
+          learning and computer programming/software engineering.
+        </p>
+        <p>
+          I then went on to study physics and eventually recieved my Masters in Science for Physics at 
+          Northeastern University while reseaching with the CMS group with a focus on machine learning 
+          and large data analytics. This naturally exposed me to the experience I would need to then get 
+          a job as a Software Development Engineer at Blue Origin, which I currently hold. My current job 
+          title is a full stack developer for the Enterprise Manufacturing Execution team. As such, I 
+          utilize various programming languages to fix and improve all aspects of web applications from 
+          front-to-backend. While working and learning as a new software engineer, I will continue to 
+          push my boundaries and learn all that I can within the field of full stack development.
+        </p>
+        <p>
+          Outside of education and work, I love to play golf (when it's not raining), hike with my wife 
+          and giant dog, visit local breweries with friends, and play video games (favorites are Elden 
+          Ring and Halo Infinite at the moment).
+        </p>
+      </div>
     </div>
   );
 }
